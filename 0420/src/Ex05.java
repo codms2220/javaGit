a58bc0cb08aa144b438f3f32dff573d54563166d
305 <305@305-PC>
2020-04-20 12:02:43 +0900

    ex05 change

--- 0420/src/Ex05.java
+++ 0420/src/Ex05.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
 
 public class Ex05 {
 
 	public static void main(String[] args) {
+
 		System.out.println("스위치 공부해야됨");
+		
+		System.out.println("숫자를 입력하세요.");
+
+		Scanner scan = new Scanner(System.in);
+		int input = scan.nextInt();
+
+		// 자동 정렬 ctrl + shift + f
+		switch (input) {
+		case 1:
+			System.out.println("1 입력");
+			break;
+		case 2:
+			System.out.println("2 입력");
+			break;
+		case 3:
+			System.out.println("3 입력");
+			break;
+		default:
+			System.out.println("다른거 입력");
+			break;
+		}
 	}
 }
