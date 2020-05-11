aa30ab9578cb134340c3fd4e0fa610419d6d46ef
codms2220 <codms2220@naver.com>
2020-05-11 17:52:09 +0900

    0511

--- 0511/src/Test01.java
+++ 0511/src/Test01.java
@@ -3,10 +3,13 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		String pnum = "990925-1012999";
-		String st1 = pnum.substring(0, 6);
-		String st2 = pnum.substring(7);
 		
-		System.out.println(st1+" "+st2);
+		pnum = pnum.replace("-", " ");
+		System.out.println(pnum);
+//		String st1 = pnum.substring(0, 6);
+//		String st2 = pnum.substring(7);
+		
+//		System.out.println(st1+" "+st2);
 	}
 	
 }
